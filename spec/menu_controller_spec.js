29b0422ee49fede074a50095dee1f360f92cfad0
Benjamin <bnreinecke0209@gmail.com>
2018-10-29 11:08:10 -0400

    Assignment: Testing: Completed

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -22,4 +22,15 @@ const MenuController = require("../controllers/MenuController");
 
 
    });
+
+
+   describe("#remindMe()", () => {
+     it("should return 'Learning is a life-long pursuit'", () => {
+       expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit");
+     })
+
+
+   });
+
+
  });
