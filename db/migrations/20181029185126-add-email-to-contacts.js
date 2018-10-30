e210e2d8632d326239e622c652093fc08d5037ec
Benjamin <bnreinecke0209@gmail.com>
2018-10-30 10:53:46 -0400

    Checkpoint: Creating Contacts and Connecting to a Database: 3/4 done

--- db/migrations/20181029185126-add-email-to-contacts.js
+++ db/migrations/20181029185126-add-email-to-contacts.js
@@ -7,25 +7,9 @@ module.exports = {
       'email',
       Sequelize.STRING
     )
-
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.removeColumn('Contacts', 'email')
-
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
   }
 };
